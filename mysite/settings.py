cccde335ff5cbb4f9f9d8b63e7dde71523741e22
sunamoon4wind <sunamoon4wind@gmail.com>
2022-04-21 02:56:42 +0900

    test17

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,9 +130,9 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR, "static"),
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'deploy')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS =[
-   os.path.join(BASE_DIR, 'static'),
+   os.path.join(BASE_DIR, 'static_dev/'),
 ]
 
 
